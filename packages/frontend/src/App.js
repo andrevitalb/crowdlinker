4637b081b93e8587609f07628d933bafbf57c854
André Vital <santiago_vital@outlook.com>
2021-09-21 20:43:47 -0500

    Added base site structure files & routing config

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,7 +1,24 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+// Pages
+import home from './pages/home';
+import login from './pages/login';
+import signup from './pages/signup';
+
+// Util
+import AuthRoute from './util/AuthRoute';
 const App = () => {
 	return (
+			<Router>
+				<Navbar />
 				<section className="mainApp">
+					<Switch>
+						<Route exact path="/" component={home} />
+						<AuthRoute exact path="/login" component={login} />
+						<AuthRoute exact path="/signup" component={signup} />
+					</Switch>
 				</section>
+			</Router>
 	);
 };
 
---
+++ packages/frontend/src/pages/login.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Login = () => {
+	return <section className="login"></section>;
+};
+
+export default Login;
