d8293283402673c8c3e3c9db706ab44cb99208c1
André Vital <santiago_vital@outlook.com>
2021-09-23 17:27:04 -0500

    Added basic auth login/logout persistance

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import RouteController from './util/RouteController';
+import jwtDecode from 'jwt-decode';
+import axios from 'axios';
 
 // Pages
 import home from './pages/home';
@@ -12,10 +14,29 @@ import Navbar from './components/Navbar/';
 // Redux
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import { SET_AUTHENTICATED } from './redux/types';
+import { getUserData, logoutUser } from './redux/actions/userActions';
+
 // Theme
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import themeValues from './util/theme';
 const theme = createTheme(themeValues);
+
+axios.defaults.baseURL =
+	'https://us-central-crowdlinker-fe6ee.cloudfunctions.net/api';
+
+const token = localStorage.AuthToken;
+if (token) {
+	const { exp } = jwtDecode(token);
+	if (exp * 1000 < Date.now()) {
+		store.dispatch(logoutUser());
+		window.location.href = '/login';
+	} else {
+		store.dispatch({ type: SET_AUTHENTICATED });
+		axios.defaults.headers.common['Authorization'] = token;
+		store.dispatch(getUserData());
+	}
+}
 
 const App = () => {
 	return (
