84db85ca2636a1571827b8fee6fee54096427829
André Vital <santiago_vital@outlook.com>
2021-09-23 16:52:17 -0500

    Added basic Firebase config

---
+++ packages/backend/functions/util/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	apiKey: 'AIzaSyDCAdUjWLeRUWRiEjhgml1CyudKC6-YU_I',
+	authDomain: 'crowdlinker-fe6ee.firebaseapp.com',
+	projectId: 'crowdlinker-fe6ee',
+	storageBucket: 'crowdlinker-fe6ee.appspot.com',
+	messagingSenderId: '1053733458975',
+	appId: '1:1053733458975:web:0d740f9f7ea635e36f312a',
+};
